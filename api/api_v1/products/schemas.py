4e02f943b7eb630739dd60ec5840ac29a693ca4d
Pavel <savchukpa@icloud.com>
2025-08-05 13:04:47 +0300

    install alembic, init alembic, add settings, create migration products table

--- api/api_v1/products/schemas.py
+++ api/api_v1/products/schemas.py
@@ -1,13 +1,20 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
 
 
 class ProductBase(BaseModel):
-    name: str
-    description: str
-    product_code: str
-    brand: str
-    price: int
-    in_stock: int
+    name: Annotated[str, MinLen(2), MaxLen(64)] = Field(
+        description="Наименование продукта"
+    )
+    description: Annotated[str, MinLen(2), MaxLen(512)] = Field(
+        description="Описание продукта"
+    )
+    product_code: Annotated[str, MinLen(6), MaxLen(128)] = Field(
+        description="Артикул продукта"
+    )
+    brand: Annotated[str, MinLen(1), MaxLen(32)] = Field(description="Бренд продукта")
+    price: int = Field(gt=0, description="Цена должна быть положительной")
 
 
 class ProductCreate(ProductBase):
@@ -19,10 +26,9 @@ class Product(ProductBase):
     id: int
 
 
-class ProductUpdate(ProductBase):
-    name: str | None = None
-    description: str | None = None
-    product_code: str | None = None
-    brand: str | None = None
-    price: int | None = None
-    in_stock: int | None = None
+class ProductUpdate(BaseModel):
+    name: str | None = Field(default=None, min_length=2, max_length=64)
+    description: str | None = Field(default=None, min_length=2, max_length=512)
+    product_code: str | None = Field(default=None, min_length=2, max_length=32)
+    brand: str | None = Field(default=None, min_length=1, max_length=128)
+    price: int | None = Field(default=None, gt=0)
